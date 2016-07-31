af9b62edd303f2305c7c06a0f865a5094dcd4bd6
tito <tttito@gmail.com>
2016-07-31 14:38:53 +0200

    Corrected visualisation of vertex images

--- manifold_embedding/manifold_embedding_1.py
+++ manifold_embedding/manifold_embedding_1.py
@@ -15,69 +15,86 @@ test_data = pickle.load(f)[0]
 f.close()
 
 showAll = False
+plotVertexImages = True
 testlen = 5000
 start=0
 
-vals = np.array([9, 5, 1])
-colors = vals / 10.
-chosens = [index for index in range(start, start+testlen) if test_data[1][index] in vals]
+target_values = np.array([7, 1, 2])
+colors = target_values / 10.
+chosens = [index for index in range(start, start+testlen) if test_data[1][index] in target_values]
 
+indexes = np.asarray([i for i in chosens])
 X = np.asarray([test_data[0][i] for i in chosens])
 y = np.asarray([test_data[1][i] for i in chosens])
-# X = digits.data
-#
+
 n_samples, n_features = X.shape
 n_neighbors = 30
 
 #----------------------------------------------------------------------
 # Scale and visualize the embedding vectors
 def plot_embedding(X, title=None):
-    x_min, x_max = np.min(X, 0), np.max(X, 0)
-    X = (X - x_min) / (x_max - x_min)
-
     plt.figure()
     ax = plt.subplot(111)
-    points=[[],[],[]]
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+    points = [[] for k in range(len(target_values))]
     for i in range(X.shape[0]):
-        for j in range(len(vals)):
-            if (y[i] == vals[j]):
+        for j in range(len(target_values)):
+            if (y[i] == target_values[j]):
                 plt.text(X[i, 0], X[i, 1], str(y[i]),
                          color=plt.cm.Set1(colors[j]),
                          fontdict={'weight': 'bold', 'size': 9})
                 points[j].append([X[i, 0], X[i, 1]])
                 break
 
-    hulls = []
-    for j in range(len(vals)):
-        points[j] = np.asarray(points[j])
-        hull = ConvexHull(np.asarray(points[j]))
+    vertexindexes =[]
+    for j in range(len(target_values)):
+        the_points = np.asarray(points[j])
+        hull = ConvexHull(np.asarray(the_points))
         for simplex in hull.simplices:
-            plt.plot(points[j][simplex, 0], points[j][simplex, 1], color=plt.cm.Set1(colors[j]), ls='-')
-
+            plt.plot(the_points[simplex, 0], the_points[simplex, 1], color=plt.cm.Set1(colors[j]), ls='-')
+        vertexindexes.append(hull.vertices)
     plt.xticks([]), plt.yticks([])
     if title is not None:
         plt.title(title)
+    if plotVertexImages:
+        plot_vertex_images(title = "Vertex Images - "+ title, rangeimg=vertexindexes)
 
-#----------------------------------------------------------------------
-# Plot images of the digits
-# n_img_per_row = 20
-# img = np.zeros((28 * n_img_per_row, 28 * n_img_per_row))
-# for i in range(n_img_per_row):
-#     ix = 10 * i + 1
-#     for j in range(n_img_per_row):
-#         iy = 10 * j + 1
-#         img[ix:ix + 28, iy:iy + 28] = X[i * n_img_per_row + j].reshape((28, 28))
-#
-# plt.imshow(img, cmap=plt.cm.binary)
-# plt.xticks([])
-# plt.yticks([])
-# plt.title('A selection from the 64-dimensional digits dataset')
+#Plot images of the vertex digits
+def plot_vertex_images(title = None, rangeimg=None):
+    nr_vertex_images = 0
+    for k in range(len(rangeimg)):
+        nr_vertex_images += len(rangeimg[k])
+    print("nr of vertex images = " + str(nr_vertex_images))
+    breakk = False
+    n_img_per_row = 7
+    targ_val = 0
+    the_index = -1
+    img = np.zeros((28 * n_img_per_row, 28 * n_img_per_row))
+    for i in range(n_img_per_row):
+        ix = 28 * i
+        for j in range(n_img_per_row):
+            the_index += 1
+            if (the_index == len(rangeimg[targ_val])) :
+                targ_val += 1
+                the_index = 0
+            if (i * n_img_per_row + j == nr_vertex_images):
+                breakk = True
+                break
+            iy = 28 * j
+            img[ix:ix + 28, iy:iy + 28] = X[rangeimg[targ_val][the_index]].reshape((28, 28))
+        if breakk:
+            break
+    plt.figure()
+    plt.imshow(img, cmap=plt.cm.binary)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(title)
 
 
 #----------------------------------------------------------------------
 # Random 2D projection using a random unitary matrix
-if showAll == True:
-    print("Computing random projection")
+if showAll:
     rp = random_projection.SparseRandomProjection(n_components=2, random_state=42)
     X_projected = rp.fit_transform(X)
     plot_embedding(X_projected, "Random Projection of the digits")
@@ -85,7 +102,7 @@ if showAll == True:
 
 #----------------------------------------------------------------------
 # Projection on to the first 2 principal components
-if showAll == True:
+if showAll:
     print("Computing PCA projection")
     t0 = time()
     X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
@@ -95,7 +112,6 @@ if showAll == True:
 
 #----------------------------------------------------------------------
 # Projection on to the first 2 linear discriminant components
-
 print("Computing Linear Discriminant Analysis projection")
 X2 = X.copy()
 X2.flat[::X.shape[1] + 1] += 0.01  # Make X invertible
@@ -107,7 +123,7 @@ plot_embedding(X_lda,
 
 #----------------------------------------------------------------------
 # Modified Locally linear embedding of the digits dataset
-if showAll == True:
+if showAll:
     print("Computing modified LLE embedding")
     clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
                                           method='modified')
@@ -123,7 +139,7 @@ if showAll == True:
 
 #----------------------------------------------------------------------
 # MDS  embedding of the digits dataset
-if showAll == True:
+if showAll:
     print("Computing MDS embedding")
     clf = manifold.MDS(n_components=2, n_init=1, max_iter=100)
     t0 = time()
@@ -135,7 +151,7 @@ if showAll == True:
 
 #----------------------------------------------------------------------
 # Random Trees embedding of the digits dataset
-if showAll == True:
+if showAll:
     print("Computing Totally Random Trees embedding")
     hasher = ensemble.RandomTreesEmbedding(n_estimators=200, random_state=0,
                                            max_depth=5)
@@ -150,7 +166,7 @@ if showAll == True:
 
 #----------------------------------------------------------------------
 # Spectral embedding of the digits dataset
-if showAll == True:
+if showAll:
     print("Computing Spectral embedding")
     embedder = manifold.SpectralEmbedding(n_components=2, random_state=0,
                                           eigen_solver="arpack")
@@ -161,7 +177,7 @@ if showAll == True:
                    "Spectral embedding of the digits (time %.2fs)" %
                    (time() - t0))
 
-if showAll == True:
+if showAll:
     print("Computing t-SNE embedding")
     tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
     t0 = time()
