0404daa17514be880cf1b0382f23e4fb937d2b3d
tito <tttito@gmail.com>
2016-10-17 12:37:18 +0200

    Extend tests

--- playground/market/test_market_4.py
+++ playground/market/test_market_4.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import itertools
+import matplotlib.mlab as mlab
 
-seed(4890)
-init_price = 75.
+#seed(4890)
+init_price = 85.
+tot_runs = 1000
 secu = Stock(id='A', price= init_price)
 
-def create_investors(nr = 20, cash_base=80., secu = secu):
+def create_investors(nr = 20, cash_base=300., secu = secu):
     invs = {}
     for i in range(1, nr+1):
         holding = Holding(secu, nr=randint(30, 70))
@@ -31,15 +33,29 @@ def calc_tot_cash():
         tot_cash += inv.cash
     print 'Tot cash ' +str(tot_cash)
 
-
-def trade_run(investors):
+def trade_run(investors, num_sell =1 ):
     for pair in itertools.combinations(sample(investors.values(), len(investors.values())), r=2):
-            success = trade(pair[0], pair[1], Holding(secu, 1))
+            success = trade(pair[0], pair[1], Holding(secu, num_sell) )
             if success:
                 secu.price +=  secu.price/1000
             else:
                 secu.price += -secu.price/1000
-    return secu.price
+    return secu.price, success
+
+def trade_run_iterate(investors, init_price = init_price, runs = tot_runs):
+    plot_arr = [init_price]
+    num_sell = 1
+    for _ in range(runs):
+        tr = trade_run(investors, num_sell)
+        plot_arr.append(tr[0])
+        if tr[1]:
+            num_sell = 1
+        else:
+            num_sell = 2
+    return plot_arr
+
+def gauss(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 print("Init")
 calc_tot_assets()
@@ -56,10 +72,7 @@ for inv in investors.values():
 bp = plt.scatter(a,b, s=80, facecolors='none', edgecolors='r', label='Total value initial')
 cp = plt.scatter(a,c, color='r', label='Initial cash')
 
-plot_arr = [init_price]
-
-for _ in range(1000):
-    plot_arr.append(trade_run(investors))
+plot_arr = trade_run_iterate(investors, runs = tot_runs)
 
 print("Final")
 calc_tot_assets()
@@ -79,6 +92,28 @@ zp = plt.xticks(np.arange(1, len(investors) + 1, 1.0))
 plt.legend(loc = 4 ,scatterpoints = 1, prop={'size':10})
 plt.show()
 plt.plot(plot_arr)
+plt.show()
+
+from scipy.stats import kurtosistest
+
+plot_diffs = np.asarray(plot_arr)
+plot_diffs = np.roll(plot_diffs, 1) - plot_diffs
+plot_diffs[0] = 0.
+count, bins, ignored = plt.hist(plot_diffs, 100, normed=True)
+min = plot_diffs.min()
+max = plot_diffs.max()
+mu = np.mean(plot_diffs)
+variance = np.var(plot_diffs)
+sigma = np.sqrt(variance)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
+             np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+        linewidth=2, color='r')
+plt.title("Mean = "+str(mu) + " , std ="+str(sigma))
+plt.xlabel("Kurtosis test p-value = "+str(kurtosistest(plot_diffs)[1]))
+plt.ylabel("Frequency")
+
+fig = plt.gcf()
+
 plt.show()
 
 
